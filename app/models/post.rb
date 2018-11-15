01b1d232a692b8051f56dc91075bf15c9ee68241
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-15 22:37:40 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Nonfiction) }
+  validate :is_clickbait
+
+  def is_clickbait
+    if self.title
+      if ["won't believe", "secret", "Top #{/\d+/}", "guess"].any? { |clickbait| self.title.downcase.include?(clickbait) } == false
+        errors.add(:title, "Not clickbait!")
+      end
+    end
+  end
 end

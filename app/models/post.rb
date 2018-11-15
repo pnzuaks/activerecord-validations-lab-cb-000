a20d51f8702903e11f185ade79f52cbf16a1aaab
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-15 22:18:18 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :post_content, length: { minimum: 250 }
-  validates :post_summary, length: { maximum: 500 }
+  validates :post_summary, length: { maximum: 250 }
 
 end

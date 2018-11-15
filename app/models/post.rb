aa394c49b0bf93941c5920f67ebf245a04a08e32
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-15 22:26:47 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Nonfiction) }
-  validates :non_clickbait, inclusion: { in: %w(Fiction Nonfiction) }
+  validates :non_clickbait, inclusion: { in: %w( "Won't Believe", "Secret", "Top [number]", or "Guess" Nonfiction) }
 end

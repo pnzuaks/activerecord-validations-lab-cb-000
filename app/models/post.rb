9d8426f5dc5c6f304751e95084f8839f16ca1800
Patricia Nykaza <pnykaza@calstatela.edu>
2018-11-15 22:31:28 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ActiveRecord::Base
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Nonfiction) }
   validates :is_clickbait
+
+  def buis_clickbait
+    if self.title
+      if ["won't believe", "secret", "Top #{/\d+/}", "guess"].any? { |clickbait| self.title.downcase.include?(clickbait) } == false
+        errors.add(:title, "Gotta have that clickbait, bruh")
+      end
+    end
+  end
 end
